2c725325179f0b679a6471ac1e19427d169fbe8b
ppzhang123 <939223493@qq.com>
2022-04-03 23:23:44 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '栗子老板',
+    description: '...',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '立早 JavaScript 博客',
+                items: [
+                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ]
+    }
+}
